b8601ecd354053c33b6c66bf786408acab1947a0
cbrianbet <cbrianbet@outlook.com>
2024-06-03 19:37:20 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './components/Auth/Login';
-import Register from './components/Auth/Register';
-import FileUpload from './components/FileUpload/FileUpload';
-import FileDownload from './components/FileDownload/FileDownload';
-import AdminPanel from './components/AdminPanel/AdminPanel';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
+import FileUpload from "./components/FileUpload/FileUpload";
+import FileDownload from "./components/FileDownload/FileDownload";
+import AdminPanel from "./components/AdminPanel/AdminPanel";
 
 const App = () => {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/login" component={Login} />
-                <Route path="/register" component={Register} />
-                <Route path="/upload" component={FileUpload} />
-                <Route path="/download" component={FileDownload} />
-                <Route path="/admin" component={AdminPanel} />
-            </Routes>
-        </Router>
-    );
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<Navigate to="/login" />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/register" element={<Register />} />
+				<Route path="/upload" element={<FileUpload />} />
+				<Route path="/download" element={<FileDownload />} />
+				<Route path="/admin" element={<AdminPanel />} />
+			</Routes>
+		</Router>
+	);
 };
 
 export default App;
